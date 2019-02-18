7b75d75260f20587db05f627a002d41fc2a5fa07
crisian <cristianibarguen1986@gmail.com>
2019-02-17 22:59:45 -0500

    hola mundo y suma

--- src/PracticaRP.java
+++ src/PracticaRP.java
@@ -4,10 +4,13 @@ public class PracticaRP {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	int a,b,c,d;
+	int a,b,SUMA;
 	String val,val2 ;
 	
-		
+	a = 2;	
+	b = 3 ;
+	SUMA = a + b;
+	System.out.println("hola mundo "+ SUMA);
 		
 	}
 
